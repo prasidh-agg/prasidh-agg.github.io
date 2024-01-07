1174860c5fe4e5e6227677e6c0eb9c9e6c6a1d1d
prasidh-agg <71028669+prasidh-agg@users.noreply.github.com>
2024-01-07 16:21:21 +0000

    Deploying to gh-pages from @ prasidh-agg/prasidh-agg.github.io@59eb715025dc1f570c3c342a46c02aaf3223a25b 🚀

--- _next/static/kqa-sHLfpPKlMGMeMqJjH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/b637e9a5-46b9eadcbbf7c380.js","static/chunks/106-2b4da4b4561aa4b2.js","static/chunks/pages/index-e88455a1b39fae01.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
